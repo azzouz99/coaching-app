d665152638300871cf5cb6bae1e6f382c721e306
azzouz99 <abderrahmen.benazzouz@outlook.fr>
2025-07-01 14:39:03 +0100

    Make it in french 2.0

---
+++ lang/fr/auth.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+
+    // Custom translations for authentication forms
+    'Adresse Email' => 'Adresse Email',
+    'Mot De Passe' => 'Mot De Passe',
+    'Se souvenir de moi' => 'Se souvenir de moi',
+    'Mot de passe oublié ?' => 'Mot de passe oublié ?',
+    'Se connecter' => 'Se connecter',
+    'Nom' => 'Nom',
+    'Confirmer le Mot de Passe' => 'Confirmer le Mot de Passe',
+    'Déjà inscrit ?' => 'Déjà inscrit ?',
+    'S\'inscrire' => 'S\'inscrire',
+    'Nouveau Mot de Passe' => 'Nouveau Mot de Passe',
+    'Réinitialiser le Mot de Passe' => 'Réinitialiser le Mot de Passe',
+    'Mot de passe oublié ? Aucun problème. Indiquez-nous simplement votre adresse e-mail et nous vous enverrons un lien de réinitialisation de mot de passe qui vous permettra d\'en choisir un nouveau.' => 'Mot de passe oublié ? Aucun problème. Indiquez-nous simplement votre adresse e-mail et nous vous enverrons un lien de réinitialisation de mot de passe qui vous permettra d\'en choisir un nouveau.',
+    'Envoyer le Lien de Réinitialisation' => 'Envoyer le Lien de Réinitialisation',
+    'Il s\'agit d\'une zone sécurisée de l\'application. Veuillez confirmer votre mot de passe avant de continuer.' => 'Il s\'agit d\'une zone sécurisée de l\'application. Veuillez confirmer votre mot de passe avant de continuer.',
+    'Confirmer' => 'Confirmer',
+];
