84c280af7b32a744e7b66f18273cb9c5f198a51f
azzouz99 <abderrahmen.benazzouz@outlook.fr>
2025-07-24 21:57:39 +0100

    add email_verif

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,15 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, MustVerifyEmail;
+    
 
     /**
      * The attributes that are mass assignable.
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SubscriptionController;
 use App\Models\Coach;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 Route::get('/', function () {
     $coaches = Coach::all();
     return view('welcome', compact('coaches'));
@@ -38,6 +39,24 @@ Route::get('/course/{course}', function (\App\Models\Course $course) {
 
 // Routes pour l'abonnement
 Route::middleware('auth')->group(function () {
+
+        Route::view('email/verify', 'auth.verify-email')
+         ->name('verification.notice');
+
+    // Handle the link from the user’s email
+    Route::get('email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+        return redirect()->intended('/dashboard');
+    })->middleware(['signed'])->name('verification.verify');
+
+    // Resend verification link
+    Route::post('email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+        return back()->with('status', 'verification-link-sent');
+    })->middleware('throttle:6,1')->name('verification.send');
+
+
+    // ************************ Inscription au congrés ***********************************************
     Route::get('/subscription/checkout', [SubscriptionController::class, 'checkout'])->name('subscription.checkout');
     Route::post('/subscription/process', [SubscriptionController::class, 'process'])->name('subscription.process');
     Route::get('/subscription/success', [SubscriptionController::class, 'success'])->name('subscription.success');
